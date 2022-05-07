da1a574ba3cc00a914e237b2f3300ff2d34ab6ac
赵公子 <zha0gongz1@protonmail.com>
2022-05-07 20:23:37 +0800

    Add files via upload

---
+++ encrypt/encryptUrl.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var XorKey []byte = []byte{0x32, 0x34, 0x85, 0x6A, 0xA3, 0xFF, 0xF4, 0x7B}
+var result string
+
+func main() {
+	j := 0
+	s := ""
+	src := "http://192.168.0.100:8080/zha0gongz1"
+	bt := []rune(src)
+	for i := 0; i < len(bt); i++ {
+		s = strconv.FormatInt(int64(byte(bt[i])^XorKey[j]), 16)
+		if len(s) == 1 {
+			s = "0" + s
+		}
+		result = result + (s)
+		j = (j + 1) % 8
+	}
+	fmt.Println(result)
+
+}
